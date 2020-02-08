83ec4ffba1ad7920d413a47e0e2e9ab40dd1c79b
Francois MUGOROZI <francoismugorozi@gmail.com>
2020-02-07 21:52:52 -0800

    User can view bank account, Create account and view account transactions
    Added the client dashboard where he can view bank accounts, create account and view transactions
    
    #171102200

---
+++ UI/js/client.js
@@ -0,0 +1,112 @@
+// All the DOM elements needed
+const menu = document.getElementById('menu');
+const sidebar = document.getElementById('sidebar');
+const main = document.getElementById('main');
+const userSideAccountItem = document.getElementById('user-side-account');
+const accounts = document.getElementById('accounts');
+const transaction = document.getElementById('transactions');
+const userSideTransactionItem = document.getElementById(
+  'user-side-transaction'
+);
+const userMenuHeading = document.getElementById('User-content-heading');
+const createAccountModel = document.getElementById('create-account-model');
+const accountDetailModel = document.getElementById('account-detail-model');
+const userProfile = document.getElementById('user-profile');
+
+// first setups when document loaded
+initialSetup = () => {
+  if (window.innerWidth < 768) {
+    sidebar.classList.remove('sidebar');
+    sidebar.classList.add('sidebar-hide');
+    main.classList.remove('main-content');
+    main.classList.add('main-full');
+  }
+  // Get the accounts from database
+
+  // populate the account div
+
+  // display the account div
+  userMenuHeading.innerHTML = 'Accounts';
+  accounts.style.display = 'block';
+  transaction.style.display = 'none';
+  userSideAccountItem.style.backgroundColor = 'rgb(10, 46, 46)';
+  userSideTransactionItem.style.background = 'none';
+};
+
+// handle the menu button sidebar
+menu.addEventListener('click', ev => {
+  if (sidebar.className === 'sidebar') {
+    sidebar.classList.remove('sidebar');
+    sidebar.classList.add('sidebar-hide');
+    main.classList.remove('main-content');
+    main.classList.add('main-full');
+  } else {
+    sidebar.classList.remove('sidebar-hide');
+    sidebar.classList.add('sidebar');
+    main.classList.remove('main-full');
+    main.classList.add('main-content');
+  }
+});
+
+// Handle List item click fo user menu
+userSideAccountItem.addEventListener('click', ev => {
+  // Get the accounts from database
+
+  // populate the account div
+
+  // display the account div
+  userMenuHeading.innerHTML = 'Accounts';
+  accounts.style.display = 'block';
+  transaction.style.display = 'none';
+  userSideAccountItem.style.backgroundColor = 'rgb(10, 46, 46)';
+  userSideTransactionItem.style.background = 'none';
+});
+userSideTransactionItem.addEventListener('click', ev => {
+  // Get the transaction from database
+
+  // populate the transaction div
+
+  // display the account div
+  userMenuHeading.innerHTML = 'Transactions';
+  accounts.style.display = 'none';
+  transaction.style.display = 'block';
+  userSideAccountItem.style.background = 'none';
+  userSideTransactionItem.style.backgroundColor = 'rgb(10, 46, 46)';
+});
+
+// Create account model handle
+const openCreateAccountModel = () => {
+  createAccountModel.style.display = 'block';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = event => {
+//   if (event.target == createAccountModel) {
+//     createAccountModel.style.display = 'none';
+//   }
+//   if (event.target == accountDetailModel) {
+//     accountDetailModel.style.display = 'none';
+//   }
+// };
+
+const openDetailModel = () => {
+  accountDetailModel.style.display = 'block';
+};
+
+const closeDetailModel = () => {
+  accountDetailModel.style.display = 'none';
+};
+const closeCreateAccountModel = () => {
+  createAccountModel.style.display = 'none';
+};
+
+// user profile open
+const openUser = () => {
+  if (userProfile.className === 'user-profile') {
+    userProfile.classList.remove('user-profile');
+    userProfile.classList.add('user-profile-hide');
+  } else {
+    userProfile.classList.remove('user-profile-hide');
+    userProfile.classList.add('user-profile');
+  }
+};
---
+++ UI/js/logout.js
@@ -0,0 +1,6 @@
+const logout = () => {
+  // destroy user jwt and logout
+
+  // redirect to home page
+  window.location.replace('../pages/index.html');
+};
