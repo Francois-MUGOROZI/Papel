f15bae8e6b9f5831a5f114fdf628fefe800082b5
Francois MUGOROZI <francoismugorozi@gmail.com>
2020-02-11 11:55:59 -0800

    COnfigured the Development environment, eslint,prettier and babel and Postgres on heroku even on local

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import app from './app';
 
-// create server
+// create server which listen to host or 8000
 app.listen(process.env.PORT || 8000, () => {
   console.log('Papel Server is Listeneing');
 });
