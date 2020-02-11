7b95c25044f787102ceb18a71d0ea1230f8ac41c
Francois MUGOROZI <francoismugorozi@gmail.com>
2020-02-11 11:30:26 -0800

    COnfiguring node for backend
    I have configured node,express,eslint,babel and postgresSql
    
    #171204950

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import app from './app';
 
 // create server
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Papel Server is Listeneing');
 });
