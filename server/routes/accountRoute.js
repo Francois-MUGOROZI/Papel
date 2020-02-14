3f7cb59940b50cde0a7a9a633b99e658abaf70af
Francois MUGOROZI <francoismugorozi@gmail.com>
2020-02-14 15:28:51 -0800

    Endpoint for viewing bank accounts
    #171278117

--- server/routes/accountRoute.js
+++ server/routes/accountRoute.js
@@ -9,9 +9,9 @@ import { validateAccount } from '../middleware/validator';
 import verifyToken from '../middleware/verifyToken';
 
 const accountRouter = exporess.Router();
-accountRouter.post('/create', verifyToken, validateAccount, createAccount);
-accountRouter.get('/', verifyToken, viewAccounts);
-accountRouter.get('/:status', verifyToken, viewActiveDormant);
-accountRouter.get('/user/:owner', verifyToken, viewSpecAccount);
+accountRouter.post('/create', verifyToken, validateAccount, createAccount); // creating account
+accountRouter.get('/', verifyToken, viewAccounts); // get all accounts
+accountRouter.get('/:status', verifyToken, viewActiveDormant); // get account by status
+accountRouter.get('/user/:owner', verifyToken, viewSpecAccount); // get account by owner id
 
 export default accountRouter;
