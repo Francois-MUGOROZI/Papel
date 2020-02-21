d15bbbc8fc2c4299a4cb55e5be83a54bb6e9b226
Francois MUGOROZI <francoismugorozi@gmail.com>
2020-02-20 18:00:40 -0800

    Started connecting

--- UI/js/login.js
+++ UI/js/login.js
@@ -6,9 +6,13 @@ loginForm.addEventListener('submit', e => {
   e.preventDefault();
   const username = e.target['username'].value;
   const password = e.target['password'].value;
-
-  // Validate the data and check if user exisist in database
-
-  // route to apropriate page depending on user type
-  window.location.replace('../pages/client.html');
+  console.log(username, password);
+  axios
+    .post('https://papel-andela.herokuapp.com/api/auth/login', {
+      email: username,
+      password: password
+    })
+    .then(res => {
+      console.log(res);
+    });
 });
