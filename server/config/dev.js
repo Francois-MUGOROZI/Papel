e1c5ee2b98ae2faa9d02036e5df88a7d0844e96a
Francois MUGOROZI <53614123+Francois-MUGOROZI@users.noreply.github.com>
2020-02-21 02:53:22 -0800

    Delete dev.js

--- server/config/dev.js
+++
@@ -1,7 +0,0 @@
-const devKeys = {
-  JWT_SECRETE: 'hddhgkks-dhjhjds-sjhdjgkdf-dshhjhk',
-  FROM_EMAIL: 'francoismugorozi@gmail.com',
-  FROM_PASSWORD: 'specialmagicELO'
-};
-
-export default devKeys;
